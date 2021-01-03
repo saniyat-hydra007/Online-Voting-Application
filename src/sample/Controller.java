3129c241e625ae8c8b34e8a3ec6cc36d44d5de71
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-03 22:40:27 +0600

    tf1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,11 @@
 package sample;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class Controller {
