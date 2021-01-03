aef458f012a45bf51f832c2c7d23ff53c4e71aad
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-03 22:26:26 +0600

    trying switching multiple scene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Voting Machine");
         primaryStage.setScene(new Scene(root, 484, 552));
         primaryStage.show();
     }
