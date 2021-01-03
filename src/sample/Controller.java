25f992b252cb92d65c374cebc8d855df8cfbbf94
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-04 00:23:27 +0600

    add a textfield

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
+    @FXML
+    public TextField tf1;
 
-    public void changeScreenButtonPushed(javafx.event.ActionEvent event) throws IOException {
+    @FXML
+    public TextArea ta1;
+
+    public void on(){
+        String data = tf1.getText();
+        ta1.setText(data);
+
+    }
+
+    public void next(javafx.event.ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("scene1.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
 
@@ -20,4 +34,5 @@ public class Controller {
         window.setScene(tableViewScene);
         window.show();
     }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Voting Machine");
+        primaryStage.setTitle("hello world");
         primaryStage.setScene(new Scene(root, 484, 552));
         primaryStage.show();
     }
