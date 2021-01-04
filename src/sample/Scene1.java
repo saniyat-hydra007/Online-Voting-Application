33daf78021e3e765e18b9d367f326817d8ea82a4
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-04 10:10:13 +0600

    scene1

--- src/sample/Scene1.java
+++ src/sample/Scene1.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Scene1 {
+
+    @FXML
+    public TextArea ta2;
+
+    public void vote1() {
+        ta2.setText("1");
+    }
+
+    public void vote2() {
+        ta2.setText("1");
+    }
+
+    public void vote3() {
+        ta2.setText("1");
+    }
+
 }
