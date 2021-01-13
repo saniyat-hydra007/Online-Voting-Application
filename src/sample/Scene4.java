9ef357b8721b47dea31670c26fe25a0473faf532
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-13 18:20:53 +0600

    bug in result fxml

--- src/sample/Scene4.java
+++ src/sample/Scene4.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Scene4 {
+
+    @FXML
+    TextArea resultTa1;
+
+    @FXML
+    TextArea resultTa2;
+
+    @FXML
+    TextArea resultTa3;
+
+
 }
+
