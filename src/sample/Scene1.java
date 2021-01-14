e239f5d0a6be026d209081faeacf90bbf0849971
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-14 23:46:15 +0600

    voting count

--- src/sample/Scene1.java
+++ src/sample/Scene1.java
@@ -8,7 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Scene1 {
 
@@ -22,7 +24,31 @@ public class Scene1 {
         window.setScene(tableViewScene);
         window.show();
 
-        
+        try {
+            FileInputStream f = new FileInputStream("c.txt");
+            Scanner reader2 = new Scanner(f);
+
+            String search = reader2.toString();
+
+            while (reader2.hasNextLine()){
+
+                String s = reader2.nextLine();
+                String str[] = s.split(" ");
+
+                System.out.println(str[0]);
+
+                int i=Integer.parseInt(str[0]);
+                i++;
+                System.out.println(i);
+
+                String s2=String.valueOf(i);
+
+            }
+            reader2.close();
+        } catch(IOException exception){
+            exception.printStackTrace();
+        }
+
     }
 
     public void vote2(javafx.event.ActionEvent event) throws IOException {
