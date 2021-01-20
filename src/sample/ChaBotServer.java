d6d3bc0618dfff4a9604457616f4336803f85444
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-21 00:17:00 +0600

    Update ChaBotServer.java

--- src/sample/ChaBotServer.java
+++ src/sample/ChaBotServer.java
@@ -8,6 +8,50 @@ import java.util.ArrayList;
 class ChatBot {
     public static void main(String[] args) {
         try {
+            ServerSocket server = new ServerSocket(4000);
+            System.out.println("Server Connected");
+            Socket socket = server.accept();
+            System.out.println("Connected to Client");
+
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+
+            OutputStreamWriter o = new OutputStreamWriter(socket.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(o);
+            writer.write("Bot-> Hello sir. For any information:" + "\n" + "1. Type- 'How to vote' for voting procedude" + "\n");
+            String line = reader.readLine();
+            while(line != null){
+                System.out.println(line);
+
+                writer.write(line + "\n");
+                if(line.equals("hello")){
+                    writer.write("Bot-> Hello,How Can I help You?" + "\n");
+                }
+                if(line.equals("hi")){
+                    writer.write("Bot-> Hello,How Can I help You?" + "\n");
+                }
+                if(line.equals("how to vote")){
+                    writer.write("Bot-> select" + "\n");
+                }
+                if(line.equals("How to see my information")){
+                    writer.write("Bot->Enter your name" + "\n");
+                }
+                if(line.equals("hi")){
+                    writer.write("Bot-> Hello,How Can I help You?" + "\n");
+                }
+                writer.flush();
+                line = reader.readLine();
+
+            }
+
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*public static void main(String[] args) {
+        try {
             ServerSocket server = new ServerSocket(5000);
             System.out.println("Server Started");
             ArrayList<ChatBotServerClient> chatBotServerClients = new ArrayList<>();
@@ -27,5 +71,5 @@ class ChatBot {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
