b796bdd5e5057c918b052a13bb458864988bbe73
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-29 15:42:01 +0600

    Update ChaBotServer.java

--- src/sample/ChaBotServer.java
+++ src/sample/ChaBotServer.java
@@ -50,26 +50,4 @@ class ChatBot {
             e.printStackTrace();
         }
     }
-    /*public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(5000);
-            System.out.println("Server Started");
-            ArrayList<ChatBotServerClient> chatBotServerClients = new ArrayList<>();
-
-            while (true) {
-                try {
-                    Socket socket = server.accept();
-                    ChatBotServerClient chatBotServerClient = new ChatBotServerClient(socket, chatBotServerClients);
-                    chatBotServerClients.add(chatBotServerClient);
-                    Thread clientThread = new Thread(chatBotServerClient);
-                    clientThread.start();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
