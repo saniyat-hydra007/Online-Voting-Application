c2196cdb54513a48f9af2da941309bf2d9f3a214
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-01-29 16:02:25 +0600

    Update ChaBotServer.java

--- src/sample/ChaBotServer.java
+++ src/sample/ChaBotServer.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-class ChatBot {
+class ChatBotServer {
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(4000);
@@ -18,7 +18,7 @@ class ChatBot {
 
             OutputStreamWriter o = new OutputStreamWriter(socket.getOutputStream());
             BufferedWriter writer = new BufferedWriter(o);
-            writer.write("Bot-> Hello sir. For any information type:" + "\n" + "1.'how to vote' for voting procedude" + "\n");
+            writer.write("Bot-> Hello sir. For any information type:" + "\n" + "1.'how to vote' for voting procedude" + "\n" + "1.'how to see my information' for see your information");
             String line = reader.readLine();
             while(line != null){
                 System.out.println(line);
@@ -34,10 +34,10 @@ class ChatBot {
                     writer.write("Bot-> First type your NID card number properly." + "\n" + "Then Click on 'Vote', and this will open a page with list of candidates." + "\n" + "Select your favourable candidate sign and press it. Then your vote is done" + "\n");
                 }
                 if(line.equals("how to see my information")){
-                    writer.write("Bot->Enter your name" + "\n");
+                    writer.write("Bot->Enter your name" + "\n" + "Click the button 'Information', it will show all your information.");
                 }
                 if(line.equals("how many times can i vote?")){
-                    writer.write("Bot-> Hello,How Can I help You?" + "\n");
+                    writer.write("Bot-> You can only vote for one times." + "\n");
                 }
                 writer.flush();
                 line = reader.readLine();
